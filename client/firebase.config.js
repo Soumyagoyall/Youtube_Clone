940217fe42f1aa9a3988254599662cd2b87db284
Soumya Goyal <201171@juitsolan.in>
2023-08-29 22:57:18 +0530

    project commit

---
+++ client/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA9RWUAFtR_gGl5cPOABOsAsTkdlGK_uDU",
+  authDomain: "otp-project-3b178.firebaseapp.com",
+  projectId: "otp-project-3b178",
+  storageBucket: "otp-project-3b178.appspot.com",
+  messagingSenderId: "235652136421",
+  appId: "1:235652136421:web:a5da543c197a85b6c8430c",
+  measurementId: "G-56WJWR0K6P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
